0054fe5b2515c5a1b3ad6bd9df240f7e349a09fc
m-jalali-25-36 <m.jalali.25.36@gamil.com>
2021-11-09 16:42:10 +0330

    The beginning of the development of the game class. Define functions and attributes

--- class/game.cpp
+++ class/game.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "..\class\table.cpp"
 
 using namespace std;
 
@@ -6,13 +7,45 @@ using namespace std;
 class Game
 {
 private:
-   
+    int size = 4;
+    int score = 0;
+    Table<int> table;
 
 public:
     Game()
     {
-        
     }
     ~Game() {}
-    
+    bool Create()
+    {
+        table.HeightCell(3);
+        table.WidthCell(9);
+        table.Overflow(false);
+        table.TextAlign(0);
+        table.Create(size, size);
+    }
+    bool Start()
+    {
+    }
+    bool Restart()
+    {
+    }
+    bool End()
+    {
+    }
+    bool Left()
+    {
+    }
+    bool Right()
+    {
+    }
+    bool Top()
+    {
+    }
+    bool Bottom()
+    {
+    }
+    bool GameOver()
+    {
+    }
 };
